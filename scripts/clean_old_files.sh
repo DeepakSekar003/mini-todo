29bdb2da17b3b6104cab217def510a1c9111cc9c
DeepakSekar003 <deepaksekar003@gmail.com>
2026-01-13 14:35:26 +0530

    file updated

--- scripts/clean_old_files.sh
+++ scripts/clean_old_files.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo "Cleaning old files from deployment directory..."
-rm -rf /home/ubuntu/mini-todo/*
+set -e
+
+APP_DIR="/var/www/html"
+
+if [ -d "$APP_DIR" ]; then
+  echo "Cleaning old files..."
+  sudo rm -rf ${APP_DIR:?}/*
+else
+  echo "Directory does not exist, skipping cleanup"
+fi
